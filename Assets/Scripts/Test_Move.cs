d65be6db2001bfb48d4d65adda5dddb66d4196eb
JHuiJung <wjdgmlwnd12@naver.com>
2023-06-28 13:57:12 +0900

    복구

--- Assets/Scripts/Test_Move.cs
+++ Assets/Scripts/Test_Move.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Test_Move : MonoBehaviour
 {
+    public Camera Player_Camera;
     public float speed = 8f;
+    public float turnSpeed = 8.0f;
     float verical;
+    private float xRotate, yRotate, xRotateMove, yRotateMove;
     float horizon;
-    private float xRotate, yRotate = 0f;
-    // Start is called before the first frame update
+
     void Start()
     {
         
@@ -19,18 +22,36 @@ public class Test_Move : MonoBehaviour
     {
         Move_input();
         Move();
+        Camera_Move();
     }
-
     void Move_input()
     {
         verical = Input.GetAxis("Vertical");
         horizon = Input.GetAxis("Horizontal");
 
     }
-
-    void Move()
-    {
-
+    void Move() {
         this.transform.position += new Vector3(horizon * speed * Time.deltaTime, 0, verical * speed * Time.deltaTime);
     }
+    
+
+    void Camera_Move()
+    {
+        float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime;
+        
+
+        yRotate = Mathf.Clamp(yRotate + yRotateSize, -30, 30);
+        
+        float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
+        
+        xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
+
+        
+        Player_Camera.transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+
+    }
+
+
 }
+
+
